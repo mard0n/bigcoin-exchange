debcd00efe2bcac755b64fcac55eb581a2b34f71
mard-one <mardon.mashrabov@gmail.com>
2022-01-29 20:12:43 +0500

    Initial Setup

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,13 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit
+          <code>
+            <code>axsad</code>
+            <code>sada</code>
+            src/App.tsx
+          </code>
+          and save to reload.
         </p>
         <a
           className="App-link"
