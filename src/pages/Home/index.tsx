1edeeda25c8b1ed4596468ca33ea16396fbe0619
mard-one <mardon.mashrabov@gmail.com>
2022-01-30 19:38:43 +0500

    Homepage table. removed pagination. implemented infinite scroll

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,22 +1,14 @@
 import React, { FC, useState } from 'react';
-import { useQuery } from 'react-query';
-import { useSearchParams } from 'react-router-dom';
-import Pagination from '../../components/Pagination';
+import { useInfiniteQuery } from 'react-query';
+// import { useSearchParams } from 'react-router-dom';
+// import Pagination from '../../components/Pagination';
 
-const fetchCoinsData = async (
-  currentPageNumber: number = 1,
-  coinsPerPage: number = 10,
-) => {
+const fetchCoinsData = async ({ pageParam = 0 }) => {
+  console.log('pageParam', pageParam);
+
   const res = await fetch(
-    `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=${coinsPerPage}&page=${currentPageNumber}&sparkline=false`,
+    `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=25&page=${pageParam}&sparkline=false`,
     {},
-  );
-  return res.json();
-};
-
-const fetchCoinsList = async () => {
-  const res = await fetch(
-    'https://api.coingecko.com/api/v3/coins/list?include_platform=false',
   );
   return res.json();
 };
@@ -24,33 +16,20 @@ const fetchCoinsList = async () => {
 interface HomeProps {}
 
 const Home: FC<HomeProps> = () => {
-  const coinsPerPage = 25;
-  const [pageParams, setPageParams] = useSearchParams();
-  const currentPageNumber = parseInt(pageParams.get('page') || '1', 10);
   const [tableSortType, setTableSortType] = useState<
     'name_asc' | 'name_desc' | 'price_asc' | 'price_desc' | ''
   >('');
-  const { data: coinList = [] } = useQuery('coins-list', fetchCoinsList, {
-    staleTime: 60000,
-  }); // TODO: handle error
-  const { status, data, error } = useQuery(
-    ['coins-data', currentPageNumber, coinsPerPage],
-    () => fetchCoinsData(currentPageNumber, coinsPerPage),
+  const { status, data, fetchNextPage, error } = useInfiniteQuery(
+    ['coins-data'],
+    fetchCoinsData,
     {
       staleTime: 60000,
+      getNextPageParam: (_lastPage, pages) => {
+        return pages.length + 1;
+      },
     },
   );
 
-  const handlePageClick = (pageNumber: number) => {
-    setPageParams({ page: pageNumber.toString() });
-  };
-  const handlePreviousClick = (pageNumber: number) => {
-    setPageParams({ page: pageNumber.toString() });
-  };
-  const handleNextClick = (pageNumber: number) => {
-    setPageParams({ page: pageNumber.toString() });
-  };
-  console.log('tableSortType', tableSortType);
   console.log('data', data);
 
   const handleNameSort = () => {
@@ -80,6 +59,10 @@ const Home: FC<HomeProps> = () => {
     }
   };
 
+  // const handleShowMoreCoins = () => {
+  //   setPage((state) => state + 1);
+  // };
+
   if (status === 'loading') {
     return <span>Loading...</span>;
   }
@@ -98,7 +81,8 @@ const Home: FC<HomeProps> = () => {
           </tr>
         </thead>
         <tbody>
-          {data
+          {data?.pages
+            .flat()
             .sort((a: any, b: any) => {
               switch (tableSortType) {
                 case 'name_asc':
@@ -135,13 +119,9 @@ const Home: FC<HomeProps> = () => {
             })}
         </tbody>
       </table>
-      <Pagination
-        numberOfPages={Math.floor(coinList.length / coinsPerPage)}
-        currentPageNumber={currentPageNumber}
-        handlePageClick={handlePageClick}
-        handlePreviousClick={handlePreviousClick}
-        handleNextClick={handleNextClick}
-      />
+      <button type="button" onClick={() => fetchNextPage()}>
+        Load more
+      </button>
     </>
   );
 };
